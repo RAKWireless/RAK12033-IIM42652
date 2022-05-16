96b7647e1b8ec12b97190794cf7d2d7189d9d909
beegee-tokyo <bernd.giesecke@rakwireless.com>
2022-05-16 13:17:09 +0800

    Add the sources

--- README.md
+++ README.md
@@ -1,2 +1,276 @@
-# RAK12033-IIM42652
-Library for the WisBlock 6DOF RAK12033 module, based on TDK IIM42652
+| <center><img src="./assets/rakstar.jpg" alt="RAKstar" width=25%></center>  | ![RAKWireless](./assets/RAK-Whirls.png) | [![Build Status](https://github.com/RAKWireless/RAK2033-IIM42652/workflows/RAK%20Library%20Build%20CI/badge.svg)](https://github.com/RAKWireless/RAK2033-IIM42652/actions) |
+| -- | -- | -- |
+
+# <RAK12033>
+
+RAK12033-IIM42652 is written by RAK for TDK IIM42652. It provides 6-axis data and temperature data reading, measurement range setting, threshold trigger wake-up and other functions.
+
+[*RAKWireless RAK12033*](https://store.rakwireless.com/collections/wisblock-sensor)
+
+# Documentation
+
+* **[Product Repository](https://github.com/RAKWireless/RAK12033-IIM42652)** - Product repository for the RAKWireless RAK12033 6-Axis module.
+* **[Documentation](https://docs.RAKWireless.com/Product-Categories/WisBlock/RAK12033/Overview/)** - Documentation and Quick Start Guide for the RAK12033 6-Axis module.
+
+# Installation
+
+In Arduino IDE open Sketch->Include Library->Manage Libraries then search for RAK12033.
+
+In PlatformIO open PlatformIO Home, switch to libraries and search for RAK12033.
+Or install the library project dependencies by adding
+
+```log
+lib_deps =
+  RAKWireless/RAKWireless IIM42652 6-Axis library
+```
+
+into **`platformio.ini`**
+
+For manual installation download the archive, unzip it and place the RAK12033-IIM42652 folder into the library directory.
+In Arduino IDE this is usually <arduinosketchfolder>/libraries/
+In PlatformIO this is usually <user/.platformio/lib>
+
+# Usage
+
+The library provides IIM42652 class, which allows communication with IIM42652 via IIC. These examples show how to use RAK12033.
+
+- [RAK12033_6_Axis_BasicReadings_IIM_42652](./examples/RAK12033_6_Axis_BasicReadings_IIM_42652) Get IIM-42652 sensor data and output data on the serial port.
+- [RAK12033_6_Axis_Interrupt_IIM_42652](./examples/RAK12033_6_Axis_Interrupt_IIM_42652) Trigger an interrupt when the acceleration on the axis exceeds the threshold.
+- [RAK12033_6_Axis_SetRange_IIM_42652](./examples/RAK12033_6_Axis_SetRange_IIM_42652) Setting the ACC and GYRO data output rate and measurement range.
+- [RAK12033_6_Axis_Calibration_IIM_42652](./examples/RAK12033_6_Axis_Calibration_IIM_42652) Perform zero offset calibration on the gyro output within 5 seconds after reset.  Keep the gyro absolutely still during calibration. Only the zero offset of the gyroscope is calibrated. Using the arithmetic mean calibration method.  Can be changed to a calibration method more suitable for your application.
+
+## This class provides the following methods:
+
+**bool begin(TwoWire &wirePort, uint8_t deviceAddress)**
+
+Initalizes the IIM42652 sensor.
+
+#### Parameters:
+
+| Direction | Name          | Function                                                     |
+| --------- | ------------- | ------------------------------------------------------------ |
+| in        | wirePort      | IIC interface used.                                          |
+| in        | deviceAddress | Device address should be 0x68.                               |
+| return    |               | If the device init successful return true else return false. |
+
+**void bank_selection ( uint8_t bank_sel ) **
+
+Register bank selection.
+
+#### Parameters:
+
+| Direction | Name     | Function                                                     |
+| --------- | -------- | ------------------------------------------------------------ |
+| in        | bank_sel | Selected bank.The parameters can be the following values :<br /> IIM42652_SET_BANK_0<br /> IIM42652_SET_BANK_1<br /> IIM42652_SET_BANK_2<br /> IIM42652_SET_BANK_3<br /> IIM42652_SET_BANK_0 |
+| return    |          | none                                                         |
+
+**void get_device_id ( uint8_t *device_id ) **
+
+Get device ID.
+
+#### Parameters:
+
+| Direction | Name      | Function                    |
+| --------- | --------- | --------------------------- |
+| in        | device_id | Device ID variable pointer. |
+| return    |           | none                        |
+
+**void temperature_enable ( void ) **
+
+Enable IIM42652 temperature measurement.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void temperature_disable( void ) **
+
+Disable IIM42652 temperature measurement.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void gyroscope_enable ( void )**
+
+Enable IIM42652 gyroscope measurement.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void gyroscope_disable( void )**
+
+Disable IIM42652 gyroscope measurement.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void accelerometer_enable ( void )**
+
+Enable IIM42652 acceleration measurement.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void accelerometer_disable( void )**
+
+Disable IIM42652 acceleration measurement.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void idle( void )**
+
+IDLE mode. When Accel and Gyro are powered off. The chip will go to OFF state, since the RC oscillator will also be powered off.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void ex_idle( void )**
+
+Exit IDLE mode. The RC oscillator is powered on even if Accel and Gyro are powered off.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void soft_reset( void )**
+
+Software reset IIM42652.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
+
+**void get_accel_data ( IIM42652_axis_t *accel_data ) **
+
+Get IIM42652 acceleration data.
+
+#### Parameters:
+
+| Direction | Name       | Function                                                     |
+| --------- | ---------- | ------------------------------------------------------------ |
+| in        | accel_data | Pointer to data of type .@ IIM42652_axis_t.<br />typedef struct <br/>{<br/>  int16_t x;<br/>  int16_t y;<br/>  int16_t z;<br/>} IIM42652_axis_t; |
+| return    |            | none                                                         |
+
+**void get_gyro_data( IIM42652_axis_t *gyro_data) **
+
+Get IIM42652 gyroscope data.
+
+#### Parameters:
+
+| Direction | Name      | Function                                                     |
+| --------- | --------- | ------------------------------------------------------------ |
+| in        | gyro_data | Pointer to data of type .@ IIM42652_axis_t.<br />typedef struct <br/>{<br/>  int16_t x;<br/>  int16_t y;<br/>  int16_t z;<br/>} IIM42652_axis_t; |
+| return    |           | none                                                         |
+
+**void get_temperature ( float *temperature )  **
+
+Get IIM42652 temperature data.
+
+#### Parameters:
+
+| Direction | Name        | Function                         |
+| --------- | ----------- | -------------------------------- |
+| in        | temperature | Pointer to temperature variable. |
+| return    |             | none                             |
+
+**void set_accel_fsr(IIM42652_ACCEL_CONFIG0_FS_SEL_t accel_fsr_g)**
+
+Set IIM42652 ACC full scale range.
+
+#### Parameters:
+
+| Direction | Name        | Function                                                     |
+| --------- | ----------- | ------------------------------------------------------------ |
+| in        | accel_fsr_g | Full scal range value. Reference@ IIM42652_ACCEL_CONFIG0_FS_SEL_t. |
+| return    |             | none                                                         |
+
+**void set_accel_frequency(const IIM42652_ACCEL_CONFIG0_ODR_t frequency)**
+
+Set IIM42652 ACC output data rate.
+
+#### Parameters:
+
+| Direction | Name      | Function                                                     |
+| --------- | --------- | ------------------------------------------------------------ |
+| in        | frequency | Output data rate value. Reference@ IIM42652_ACCEL_CONFIG0_ODR_t. |
+| return    |           | none                                                         |
+
+**void set_gyro_fsr(IIM42652_GYRO_CONFIG0_FS_SEL_t gyro_fsr_dps)**
+
+Set IIM42652 GYRO full scale range.
+
+#### Parameters:
+
+| Direction | Name         | Function                                                     |
+| --------- | ------------ | ------------------------------------------------------------ |
+| in        | gyro_fsr_dps | Full scal range value. Reference@ IIM42652_GYRO_CONFIG0_FS_SEL_t. |
+| return    |              | none                                                         |
+
+**void set_gyro_frequency(const IIM42652_GYRO_CONFIG0_ODR_t frequency)**
+
+Set IIM42652 GYRO output data rate.
+
+#### Parameters:
+
+| Direction | Name      | Function                                                     |
+| --------- | --------- | ------------------------------------------------------------ |
+| in        | frequency | Output data rate value. Reference@ IIM42652_GYRO_CONFIG0_ODR_t. |
+| return    |           | none                                                         |
+
+**uint8_t get_WOM_INT(void)**
+
+Get WOM interrupt flag, clears on read.
+
+#### Parameters:
+
+| Direction | Name | Function                                                     |
+| --------- | ---- | ------------------------------------------------------------ |
+| return    |      | Return the value in register INT_STATUS2. <br/>For details, refer to the description of INT_STATUS2 in the date sheet. |
+
+**void wake_on_motion_configuration(const uint8_t x_th, const uint8_t y_th, const uint8_t z_th )**
+
+Wake on motion configuration.
+
+#### Parameters:
+
+| Direction | Name | Function                                                     |
+| --------- | ---- | ------------------------------------------------------------ |
+| in        | x_th | Threshold value for the Wake on Motion Interrupt for X-axis accelerometer WoM thresholds are expressed in fixed “mg” independent of the selected Range [0g : 1g]; Resolution 1g/256=~3.9mg |
+| in        | y_th | Threshold value for the Wake on Motion Interrupt for Y-axis accelerometer WoM thresholds are expressed in fixed “mg” independent of the selected Range [0g : 1g]; Resolution 1g/256=~3.9mg |
+| in        | z_th | Threshold value for the Wake on Motion Interrupt for Z-axis accelerometer WoM thresholds are expressed in fixed “mg” independent of the selected Range [0g : 1g]; Resolution 1g/256=~3.9mg |
+| return    |      | none                                                         |
+
+**void enable_accel_low_power_mode(void)**
+
+Set ACC to low power mode.
+
+#### Parameters:
+
+| Direction | Name | Function |
+| --------- | ---- | -------- |
+| return    |      | none     |
---
+++ src/RAK12033-IIM42652.cpp
@@ -0,0 +1,601 @@
+/**
+   @file RAK12033-IIM42652.cpp
+   @author rakwireless.com
+   @brief  IIM42652 configuration function realization.
+   @version 0.1
+   @date 2022-01-01
+   @copyright Copyright (c) 2022
+**/
+
+#include "RAK12033-IIM42652.h"
+
+/*!
+ *  @brief  Initialize the class.
+ *  @param  addr: The device address of IIM42652 IIC is 0x69.
+ */
+IIM42652::IIM42652(byte addr)
+{
+  _deviceAddress = addr;
+}
+
+/*!
+ *  @brief  I2c bus write.
+ *  @param  registerAddress  : Register address.
+ *  @param  writeData        : Write data pointer.
+ *  @param  size             : The length of the written data.
+ */
+void IIM42652::writeRegister(uint8_t registerAddress, uint8_t *writeData, uint8_t size)
+{
+  _i2cPort->beginTransmission(_deviceAddress);
+  _i2cPort->write(registerAddress);
+  for (size_t i = 0; i < size; i++)
+  {
+    _i2cPort->write(writeData[i]);
+  }
+  _i2cPort->endTransmission();
+}
+
+/*!
+ *  @brief  I2c bus read.
+ *  @param  registerAddress : Register address.
+ *  @param  readData        : Read data pointer.
+ *  @param  size            : The length of the written data.
+ */
+void IIM42652::readRegister(uint8_t registerAddress, uint8_t *readData, uint8_t size)
+{
+  _i2cPort->beginTransmission(_deviceAddress);
+  _i2cPort->write(registerAddress);
+  _i2cPort->endTransmission();
+  _i2cPort->requestFrom(_deviceAddress, size);
+
+  size_t i = 0;
+  while (_i2cPort->available()) // slave may send less than requested
+  {
+    readData[i] = _i2cPort->read(); // receive a byte as a proper uint8_t
+    i++;
+  }
+}
+
+/*!
+ *  @brief  Initalizes the IIM-42652 sensor.
+ *  @param  wirePort      : IIC interface used.
+ *  @param  deviceAddress : Device address should be 0x68.
+ *  @return If the device init successful return true else return false.
+ */
+bool IIM42652::begin(TwoWire &wirePort, uint8_t deviceAddress)
+{
+  uint8_t i2cData = 0;
+  uint8_t _sensor_id;
+
+  _deviceAddress = deviceAddress;
+
+  _i2cPort = &wirePort;
+
+  LIB_LOG("begin", "RAK12033");
+  delay(100);
+  // readRegister(IIM42652_REG_WHO_AM_I , &_sensor_id , 1);
+  get_device_id(&_sensor_id);
+  LIB_LOG("begin", "ID = 0x%X", _sensor_id);
+
+  if (_sensor_id == IIM42652_CHIP_ID)
+  {
+    soft_reset();
+    return true;
+  }
+  else
+    return false;
+}
+
+/*!
+ *  @brief  Register bank selection.
+ *  @param  bank_sel	: Selected bank. eg: IIM42652_SET_BANK_0.
+ *  @return NULL.
+ */
+void IIM42652::bank_selection(uint8_t bank_sel)
+{
+  uint8_t tmp;
+
+  writeRegister(IIM42652_REG_BANK_SEL, &bank_sel, 1);
+
+  readRegister(IIM42652_REG_BANK_SEL, &tmp, 1);
+
+  LIB_LOG("bank_selection", "bank_sel = %d ", tmp);
+
+  delay(1);
+}
+
+/*!
+ *  @brief  Get device ID.
+ *  @param  device_id	: Device ID variable pointer.
+ *  @return NULL.
+ */
+void IIM42652::get_device_id(uint8_t *device_id)
+{
+  readRegister(IIM42652_REG_WHO_AM_I, device_id, 1);
+}
+
+/*!
+ *  @brief  Enable IIM42652 temperature measurement.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::temperature_enable(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp &= ~IIM42652_SET_TEMPERATURE_DISABLED;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Disable IIM42652 temperature measurement.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::temperature_disable(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp |= IIM42652_SET_TEMPERATURE_DISABLED;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  IDLE mode. When Accel and Gyro are powered off.
+            The chip will go to OFF state, since the RC oscillator will also be powered off.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::idle(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp &= 0xEF;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Exit IDLE mode. The RC oscillator is powered on even if Accel and Gyro are powered off.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::ex_idle(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp |= ~0xEF;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Enable IIM42652 gyroscope measurement.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::gyroscope_enable(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp |= IIM42652_SET_GYRO_TLOW_NOISE_MODE;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Disable IIM42652 gyroscope measurement.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::gyroscope_disable(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp &= 0xF3;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Enable IIM42652 acceleration measurement.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::accelerometer_enable(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp |= IIM42652_SET_ACCEL_LOW_NOISE_MODE;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Disable IIM42652 acceleration measurement.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::accelerometer_disable(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+
+  tmp &= 0xFC;
+
+  writeRegister(IIM42652_REG_PWR_MGMT0, &tmp, 1);
+}
+
+/*!
+ *  @brief  Software reset IIM42652.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::soft_reset(void)
+{
+  uint8_t tmp;
+
+  readRegister(IIM42652_REG_DEVICE_CONFIG, &tmp, 1);
+
+  tmp |= 0x01;
+
+  writeRegister(IIM42652_REG_DEVICE_CONFIG, &tmp, 1);
+
+  delay(10);
+}
+
+/*!
+ *  @brief  Get IIM42652 acceleration data.
+ *  @param  accel_data	:Pointer to data of type @ IIM42652_axis_t.
+ *  @return NULL.
+ */
+void IIM42652::get_accel_data(IIM42652_axis_t *accel_data)
+{
+  uint8_t rx_buf[6];
+  uint16_t tmp;
+
+  readRegister(IIM42652_REG_ACCEL_DATA_X1_UI, rx_buf, 6);
+
+  tmp = rx_buf[0];
+  tmp <<= 8;
+  tmp |= rx_buf[1];
+
+  accel_data->x = (int16_t)tmp;
+
+  tmp = rx_buf[2];
+  tmp <<= 8;
+  tmp |= rx_buf[3];
+
+  accel_data->y = (int16_t)tmp;
+
+  tmp = rx_buf[4];
+  tmp <<= 8;
+  tmp |= rx_buf[5];
+
+  accel_data->z = (int16_t)tmp;
+}
+
+/*!
+ *  @brief  Get IIM42652 gyroscope data.
+ *  @param  gyro_data	:Pointer to data of type @ IIM42652_axis_t.
+ *  @return NULL.
+ */
+void IIM42652::get_gyro_data(IIM42652_axis_t *gyro_data)
+{
+  uint8_t rx_buf[6];
+  uint16_t tmp;
+
+  readRegister(IIM42652_REG_GYRO_DATA_X1_UI, rx_buf, 6);
+
+  tmp = rx_buf[0];
+  tmp <<= 8;
+  tmp |= rx_buf[1];
+
+  gyro_data->x = (int16_t)tmp;
+
+  tmp = rx_buf[2];
+  tmp <<= 8;
+  tmp |= rx_buf[3];
+
+  gyro_data->y = (int16_t)tmp;
+
+  tmp = rx_buf[4];
+  tmp <<= 8;
+  tmp |= rx_buf[5];
+
+  gyro_data->z = (int16_t)tmp;
+}
+
+/*!
+ *  @brief  Get IIM42652 temperature data.
+ *  @param  temperature	:Pointer to temperature variable.
+ *  @return NULL.
+ */
+void IIM42652::get_temperature(float *temperature)
+{
+  uint8_t rx_buf[2];
+  int16_t tmp;
+
+  readRegister(IIM42652_REG_TEMP_DATA1_UI, rx_buf, 2);
+
+  tmp = rx_buf[0];
+  tmp <<= 8;
+  tmp |= rx_buf[1];
+
+  *temperature = (float)tmp;
+  *temperature /= 132.48;
+  *temperature += 25;
+}
+
+/*!
+ *  @brief  Set IIM42652 ACC full scale range.
+ *  @param  accel_fsr_g	:Full scal range value. Reference@ IIM42652_ACCEL_CONFIG0_FS_SEL_t.
+ *  @return NULL.
+ */
+void IIM42652::set_accel_fsr(IIM42652_ACCEL_CONFIG0_FS_SEL_t accel_fsr_g)
+{
+  uint8_t accel_cfg_0_reg;
+
+  readRegister(IIM42652_REG_ACCEL_CONFIG0, &accel_cfg_0_reg, 1);
+
+  accel_cfg_0_reg &= (uint8_t)~BIT_ACCEL_CONFIG0_FS_SEL_MASK;
+  accel_cfg_0_reg |= (uint8_t)accel_fsr_g;
+
+  writeRegister(IIM42652_REG_ACCEL_CONFIG0, &accel_cfg_0_reg, 1);
+}
+
+/*!
+ *  @brief  Set IIM42652 ACC output data rate.
+ *  @param  frequency	:Output data rate value. Reference@ IIM42652_ACCEL_CONFIG0_ODR_t.
+ *  @return NULL.
+ */
+void IIM42652::set_accel_frequency(const IIM42652_ACCEL_CONFIG0_ODR_t frequency)
+{
+  uint8_t accel_cfg_0_reg;
+
+  readRegister(IIM42652_REG_ACCEL_CONFIG0, &accel_cfg_0_reg, 1);
+
+  accel_cfg_0_reg &= (uint8_t)~BIT_ACCEL_CONFIG0_ODR_MASK;
+  accel_cfg_0_reg |= (uint8_t)frequency;
+
+  writeRegister(IIM42652_REG_ACCEL_CONFIG0, &accel_cfg_0_reg, 1);
+}
+
+/*!
+ *  @brief  Set IIM42652 GYRO full scale range.
+ *  @param  gyro_fsr_dps	:Full scal range value. Reference@ IIM42652_GYRO_CONFIG0_FS_SEL_t.
+ *  @return NULL.
+ */
+void IIM42652::set_gyro_fsr(IIM42652_GYRO_CONFIG0_FS_SEL_t gyro_fsr_dps)
+{
+  uint8_t gyro_cfg_0_reg;
+
+  readRegister(IIM42652_REG_GYRO_CONFIG0, &gyro_cfg_0_reg, 1);
+
+  gyro_cfg_0_reg &= (uint8_t)~BIT_GYRO_CONFIG0_FS_SEL_MASK;
+  gyro_cfg_0_reg |= (uint8_t)gyro_fsr_dps;
+
+  writeRegister(IIM42652_REG_GYRO_CONFIG0, &gyro_cfg_0_reg, 1);
+}
+
+/*!
+ *  @brief  Set IIM42652 GYRO output data rate.
+ *  @param  frequency	:Output data rate value. Reference@ IIM42652_GYRO_CONFIG0_ODR_t.
+ *  @return NULL.
+ */
+void IIM42652::set_gyro_frequency(const IIM42652_GYRO_CONFIG0_ODR_t frequency)
+{
+  uint8_t gyro_cfg_0_reg;
+
+  readRegister(IIM42652_REG_GYRO_CONFIG0, &gyro_cfg_0_reg, 1);
+
+  gyro_cfg_0_reg &= (uint8_t)~BIT_GYRO_CONFIG0_ODR_MASK;
+  gyro_cfg_0_reg |= (uint8_t)frequency;
+
+  writeRegister(IIM42652_REG_GYRO_CONFIG0, &gyro_cfg_0_reg, 1);
+}
+
+/*!
+ *  @brief  Get WOM interrupt flag, clears on read.
+ *  @param  NULL.
+ *  @return Return the value in register INT_STATUS2.
+ *			For details, refer to the description of INT_STATUS2 in the date sheet.
+ */
+uint8_t IIM42652::get_WOM_INT(void)
+{
+  uint8_t data;
+
+  readRegister(IIM42652_REG_INT_STATUS2, &data, 1);
+
+  LIB_LOG("get_WOM_INT", "IIM42652_REG_INT_STATUS2 = %d ", data);
+
+  return data;
+}
+
+/*!
+ *  @brief  Wake on motion configuration.
+ *  @param  x_th	:Threshold value for the Wake on Motion Interrupt for X-axis accelerometer
+           WoM thresholds are expressed in fixed “mg” independent of the selected
+           Range [0g : 1g]; Resolution 1g/256=~3.9mg
+ *  @param  y_th	:Threshold value for the Wake on Motion Interrupt for Y-axis accelerometer
+           WoM thresholds are expressed in fixed “mg” independent of the selected
+           Range [0g : 1g]; Resolution 1g/256=~3.9mg
+ *  @param  z_th	:Threshold value for the Wake on Motion Interrupt for Z-axis accelerometer
+           WoM thresholds are expressed in fixed “mg” independent of the selected
+           Range [0g : 1g]; Resolution 1g/256=~3.9mg
+ *  @return NULL.
+ */
+void IIM42652::wake_on_motion_configuration(const uint8_t x_th, const uint8_t y_th, const uint8_t z_th)
+{
+  uint8_t data[3];
+
+  readRegister(IIM42652_REG_INT_CONFIG, data, 1);
+  data[0] |= 0x02; // eg: BIT_INT_SOURCE1_WOM_Z_INT1_EN
+  LIB_LOG("wake_on_motion_configuration", "data[0] = 0x%X", data[0]);
+  writeRegister(IIM42652_REG_INT_CONFIG, data, 1);
+
+  // Set memory bank 4.
+  bank_selection(IIM42652_SET_BANK_4);
+
+  data[0] = x_th; /* Set X threshold */
+  data[1] = y_th; /* Set Y threshold */
+  data[2] = z_th; /* Set Z threshold */
+  writeRegister(IIM42652_REG_ACCEL_WOM_X_THR, data, 3);
+
+  readRegister(IIM42652_REG_ACCEL_WOM_X_THR, data, 1);
+
+  LIB_LOG("wake_on_motion_configuration", "IIM42652_REG_ACCEL_WOM_X_THR = %d ", data[0]);
+  LIB_LOG("wake_on_motion_configuration", "IIM42652_REG_ACCEL_WOM_X_THR = %d ", data[1]);
+  LIB_LOG("wake_on_motion_configuration", "IIM42652_REG_ACCEL_WOM_X_THR = %d ", data[2]);
+
+  delay(1);
+
+  // Set memory bank 0.
+  bank_selection(IIM42652_SET_BANK_0);
+
+  readRegister(IIM42652_REG_INT_SOURCE1, data, 1);
+  data[0] |= X_INT1_EN | Y_INT1_EN | Z_INT1_EN; // eg: BIT_INT_SOURCE1_WOM_Z_INT1_EN
+  LIB_LOG("wake_on_motion_configuration", "data[0] = 0x%X", data[0]);
+  writeRegister(IIM42652_REG_INT_SOURCE1, data, 1);
+
+  delay(100); // Wait 100 milliseconds.
+
+  // Turn on WOM feature by setting WOM_INT_MODE to 0, WOM_MODE to 1,
+  // SMD_MODE to 1 (Register 0x56h in Bank 0)
+  readRegister(IIM42652_REG_SMD_CONFIG, data, 1);
+  data[0] |= 0x05;
+  LIB_LOG("wake_on_motion_configuration", "data[0] = 0x%X", data[0]);
+  writeRegister(IIM42652_REG_SMD_CONFIG, data, 1);
+}
+
+/*!
+ *  @brief  Set ACC to low power mode.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::enable_accel_low_power_mode(void)
+{
+  uint8_t pwr_mgmt0_reg;
+
+  // Enable/Switch the accelerometer in/to low power mode.
+  readRegister(IIM42652_REG_PWR_MGMT0, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg &= ~BIT_PWR_MGMT_0_ACCEL_MODE_MASK;
+  pwr_mgmt0_reg |= IIM42652_PWR_MGMT_0_ACCEL_MODE_LP;
+  writeRegister(IIM42652_REG_PWR_MGMT0, &pwr_mgmt0_reg, 1);
+
+  // (Register 0x4Dh in Bank 0), ACCEL_LP_CLK_SEL = 0, for low power mode.
+  readRegister(IIM42652_REG_INTF_CONFIG1, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg &= ~BIT_ACCEL_LP_CLK_SEL_MASK;
+  pwr_mgmt0_reg |= IIM42652_INTF_CONFIG1_ACCEL_LP_CLK_WUOSC;
+  LIB_LOG("enable_accel_low_power_mode", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_INTF_CONFIG1, &pwr_mgmt0_reg, 1);
+
+  delay(1);
+}
+
+/*!
+ *  @brief  To do.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+void IIM42652::pedometer_configuration()
+{
+  uint8_t pwr_mgmt0_reg;
+
+  // Enable/Switch the accelerometer in/to low power mode.
+  readRegister(IIM42652_REG_PWR_MGMT0, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg &= ~BIT_PWR_MGMT_0_ACCEL_MODE_MASK;
+  pwr_mgmt0_reg |= IIM42652_PWR_MGMT_0_ACCEL_MODE_LP;
+  writeRegister(IIM42652_REG_PWR_MGMT0, &pwr_mgmt0_reg, 1);
+
+  // (Register 0x4Dh in Bank 0), ACCEL_LP_CLK_SEL = 0, for low power mode.
+  readRegister(IIM42652_REG_INTF_CONFIG1, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg &= ~BIT_ACCEL_LP_CLK_SEL_MASK;
+  pwr_mgmt0_reg |= IIM42652_INTF_CONFIG1_ACCEL_LP_CLK_WUOSC;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_INTF_CONFIG1, &pwr_mgmt0_reg, 1);
+
+  // Set DMP ODR = 50 Hz and turn on Pedometer feature (Register 0x56h in Bank 0)
+  readRegister(IIM42652_REG_APEX_CONFIG0, &pwr_mgmt0_reg, 1);
+  //  pwr_mgmt0_reg &= ~BIT_ACCEL_LP_CLK_SEL_MASK;
+  pwr_mgmt0_reg |= 0x22;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_APEX_CONFIG0, &pwr_mgmt0_reg, 1);
+  delay(1);
+
+  readRegister(IIM42652_REG_SIGNAL_PATH_RESET, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg |= 0x20;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_SIGNAL_PATH_RESET, &pwr_mgmt0_reg, 1);
+  delay(1);
+
+  // Set LOW_ENERGY_AMP_TH_SEL to 10 (Register 0x40h in Bank 4)
+  bank_selection(IIM42652_SET_BANK_4);
+  readRegister(IIM42652_REG_APEX_CONFIG1, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg |= 0xA0;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_APEX_CONFIG1, &pwr_mgmt0_reg, 1);
+
+  // Set DMP_INIT_EN to 1 (Register 0x4Bh in Bank 0)
+  bank_selection(IIM42652_SET_BANK_0);
+  readRegister(IIM42652_REG_SIGNAL_PATH_RESET, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg |= 0x40;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_SIGNAL_PATH_RESET, &pwr_mgmt0_reg, 1);
+
+  delay(50);
+
+  bank_selection(IIM42652_SET_BANK_4);
+  readRegister(IIM42652_REG_INT_SOURCE6, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg |= 0x20;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_INT_SOURCE6, &pwr_mgmt0_reg, 1);
+
+  // Disable freefall by setting FF_ENABLE to 0 (Register 0x56h in Bank 0)
+  bank_selection(IIM42652_SET_BANK_0);
+  readRegister(IIM42652_REG_APEX_CONFIG0, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg &= 0xFB;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_APEX_CONFIG0, &pwr_mgmt0_reg, 1);
+
+  readRegister(IIM42652_REG_APEX_CONFIG0, &pwr_mgmt0_reg, 1);
+  pwr_mgmt0_reg |= 0x20;
+  LIB_LOG("pedometer_configuration", "pwr_mgmt0_reg = 0x%X", pwr_mgmt0_reg);
+  writeRegister(IIM42652_REG_APEX_CONFIG0, &pwr_mgmt0_reg, 1);
+}
+
+/*!
+ *  @brief  To do.
+ *  @param  NULL.
+ *  @return NULL.
+ */
+uint16_t IIM42652::get_pedometer_data(void)
+{
+  uint8_t date[2];
+  uint16_t tmp;
+
+  readRegister(IIM42652_REG_APEX_DATA0, date, 2);
+
+  tmp = date[1];
+  tmp <<= 8;
+  tmp |= date[0];
+
+  return tmp;
+}
